366942e6568ab0db4e002344943ddd6c5e8d2e4d
mmpereira <marcelo@route.com>
2025-03-23 15:49:27 -0300

    Creates new tests

--- lib/pkg/samples/sample_01_test.go
+++ lib/pkg/samples/sample_01_test.go
@@ -11,42 +11,62 @@ import (
 )
 
 func TestSample01(t *testing.T) {
-	registry := converterdefault.NewRegistry()
-	// This addition method does not add the Generics fallback conversion functions which means the reflection fallbacks
-	// will be used.
-	registry.Add(converter.CreateHandler(funcs.FromStringToDate))
-	myConverters := converterdefault.NewConverters(registry)
 	d1 := "20240101"
-
-	date1, err := myConverters.ConvertToType(d1, reflect.TypeFor[time.Time]())
-	assert.NoError(t, err)
-	assert.Equal(t, d1, date1.(time.Time).Format("20060102"))
-
-	// Using a helper function to cast to you
-	date2, err2 := converter.To[time.Time](myConverters, d1)
-	assert.NoError(t, err2)
-	assert.Equal(t, d1, date2.Format("20060102"))
-
-	// Using a helper function to cast to you
-	pTDate3, err3 := converter.To[*time.Time](myConverters, d1)
-	assert.NoError(t, err3)
-	assert.NotNil(t, d1, pTDate3)
-	assert.Equal(t, d1, pTDate3.Format("20060102"))
-
 	d2 := "20210101"
-	date4 := time.Time{}
-	assert.NoError(t, myConverters.Convert(d2, &date4))
-	assert.Equal(t, d2, date4.Format("20060102"))
-
-	var date5 *time.Time
-	assert.NoError(t, myConverters.Convert(d2, &date5))
-	assert.Equal(t, d2, (*date5).Format("20060102"))
-
 	d3 := "19981123"
-	assert.NoError(t, myConverters.Convert(&d3, &date5))
-	assert.Equal(t, d3, (*date5).Format("20060102"))
 
-	var d4 *string // nil
-	assert.NoError(t, myConverters.Convert(d4, &date5))
-	assert.Nil(t, date5)
+	registry := converterdefault.NewRegistry()
+	t.Run("Adding converter without the Generics fallback conversion functions", func(t *testing.T) {
+		// This addition method does not add the Generics fallback conversion functions which means the reflection fallbacks
+		// will be used.
+		registry.Add(converter.CreateHandler(funcs.FromStringToDate))
+		myConverters := converterdefault.NewConverters(registry)
+
+		date1, err := myConverters.ConvertToType(d1, reflect.TypeFor[time.Time]())
+		assert.NoError(t, err)
+		assert.Equal(t, d1, date1.(time.Time).Format("20060102"))
+
+		// Using a helper function to cast to you
+		date2, err2 := converter.To[time.Time](myConverters, d1)
+		assert.NoError(t, err2)
+		assert.Equal(t, d1, date2.Format("20060102"))
+
+		date4 := time.Time{}
+		assert.NoError(t, myConverters.Convert(d2, &date4))
+		assert.Equal(t, d2, date4.Format("20060102"))
+
+		var d4 *string // nil
+		var date5 *time.Time
+		assert.Error(t, myConverters.Convert(d4, &date5))
+
+		// Because we added the conversion function without callbacks, then these conversions will fail
+		// Using a helper function to cast to you
+		_, err3 := converter.To[*time.Time](myConverters, d1)
+		assert.Error(t, err3)
+
+		assert.Error(t, myConverters.Convert(d2, &date5))
+
+		assert.Error(t, myConverters.Convert(&d3, &date5))
+	})
+
+	t.Run("Adding conversion function with fallbacks", func(t *testing.T) {
+		converter.AddHandler(registry, funcs.FromStringToDate)
+		myConverters := converterdefault.NewConverters(registry)
+
+		// Using a helper function to cast to you
+		pTDate3, err3 := converter.To[*time.Time](myConverters, d1)
+		if assert.NoError(t, err3) {
+			assert.NotNil(t, d1, pTDate3)
+			assert.Equal(t, d1, pTDate3.Format("20060102"))
+		}
+
+		var date5 *time.Time
+		if assert.NoError(t, myConverters.Convert(d2, &date5)) {
+			assert.Equal(t, d2, (*date5).Format("20060102"))
+		}
+
+		if assert.NoError(t, myConverters.Convert(&d3, &date5)) {
+			assert.Equal(t, d3, (*date5).Format("20060102"))
+		}
+	})
 }
